d2deea5df32d702cc11452bdcd9d0912f3dcf867
Flakoooo <vlasov.vadom@gmail.com>
2025-12-13 21:31:06 +0500

    belbin test mocks

--- HITSBlazor/Utils/Mocks/Tests/MockTestAnswers.cs
+++ HITSBlazor/Utils/Mocks/Tests/MockTestAnswers.cs
@@ -21,63 +21,66 @@ namespace HITSBlazor.Utils.Mocks.Tests
             // Получаем все вопросы теста
             var questions = MockTestQuestions.GetTestQuestionsByTestName(BelbinTestName);
 
-            // Получаем вопросы 1 модуля
-            var module1Questions = questions.Where(q => q.QuestionModuleNumber == 1).ToList();
-
-            // можно ответить минимум 2 балла, на вопрос,
-            // ответить можно максимум на 3 вопроса,
-            // сумма баллов должна быть 10
-
-            // получаем то, сколько вопросов было выбрано в модуле
-            var answersInModuleCount = _random.Next(1, 4);
-
-            // заранее задаем количество баллов
-            var scores = new List<int>();
-            if (answersInModuleCount == 1) scores.Add(10);
-            else if (answersInModuleCount == 2)
+            for (int i = 1; i <= 7; ++i)
             {
-                var firstAnswer = _random.Next(2, 9);
-                scores.AddRange(firstAnswer, 10 - firstAnswer);
-            }
-            else if (answersInModuleCount == 3)
-            {
-                var firstAnswer = _random.Next(2, 7);
-                var secondAnswer = _random.Next(2, 7 - firstAnswer);
-                scores.AddRange(firstAnswer, secondAnswer, 10 - firstAnswer - secondAnswer);
-            }
+                // Получаем вопросы модуля
+                var module1Questions = questions.Where(q => q.QuestionModuleNumber == i).ToList();
 
-            // находим те самые вопросы, присваиваем сразу им баллы
-            var answersInModule = new Dictionary<string, string>();
-            for (int i = 0; i < answersInModuleCount; ++i)
-            {
-                var currentQuestion = false;
+                // можно ответить минимум 2 балла, на вопрос,
+                // ответить можно максимум на 3 вопроса,
+                // сумма баллов должна быть 10
 
-                while (currentQuestion)
+                // получаем то, сколько вопросов было выбрано в модуле
+                var answersInModuleCount = _random.Next(1, 4);
+
+                // заранее задаем количество баллов
+                var scores = new List<int>();
+                if (answersInModuleCount == 1) scores.Add(10);
+                else if (answersInModuleCount == 2)
                 {
-                    var question = module1Questions[_random.Next(0, 8)];
-                    if (!answersInModule.TryGetValue(question.QuestionName, out string? _))
+                    var firstAnswer = _random.Next(2, 9);
+                    scores.AddRange(firstAnswer, 10 - firstAnswer);
+                }
+                else if (answersInModuleCount == 3)
+                {
+                    var firstAnswer = _random.Next(2, 7);
+                    var secondAnswer = _random.Next(2, 7 - firstAnswer);
+                    scores.AddRange(firstAnswer, secondAnswer, 10 - firstAnswer - secondAnswer);
+                }
+
+                // находим те самые вопросы, присваиваем сразу им баллы
+                var answersInModule = new Dictionary<string, string>();
+                for (int j = 0; j < answersInModuleCount; ++j)
+                {
+                    var currentQuestion = false;
+
+                    while (currentQuestion)
                     {
-                        answersInModule[question.QuestionName] = $"{scores[i]}";
-                        currentQuestion = true;
+                        var question = module1Questions[_random.Next(0, 8)];
+                        if (!answersInModule.TryGetValue(question.QuestionName, out string? _))
+                        {
+                            answersInModule[question.QuestionName] = $"{scores[j]}";
+                            currentQuestion = true;
+                        }
                     }
                 }
-            }
 
-            // создаем ответы на вопросы, указывая там где нужно сгенерированный ответ
-            foreach (var question in module1Questions)
-            {
-                answers.Add(
-                    new TestAnswer
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        TestName = question.TestName,
-                        User = user,
-                        QuestionName = question.QuestionName,
-                        QuestionModuleNumber = question.QuestionModuleNumber,
-                        QuestionNumber = question.QuestionNumber,
-                        Answer = answersInModule.GetValueOrDefault(question.QuestionName) ?? "0"
-                    }
-                );
+                // создаем ответы на вопросы, указывая там где нужно сгенерированный ответ
+                foreach (var question in module1Questions)
+                {
+                    answers.Add(
+                        new TestAnswer
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            TestName = question.TestName,
+                            User = user,
+                            QuestionName = question.QuestionName,
+                            QuestionModuleNumber = question.QuestionModuleNumber,
+                            QuestionNumber = question.QuestionNumber,
+                            Answer = answersInModule.GetValueOrDefault(question.QuestionName) ?? "0"
+                        }
+                    );
+                }
             }
 
             return answers;
