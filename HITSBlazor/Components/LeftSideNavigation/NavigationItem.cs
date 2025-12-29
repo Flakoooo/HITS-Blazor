ab8813106423a7d81a119b5f1bee7be3fdd4d9fd
Flakoooo <vlasov.vadom@gmail.com>
2025-12-29 22:02:03 +0500

    navigation ready????

---
+++ HITSBlazor/Components/LeftSideNavigation/NavigationItem.cs
@@ -0,0 +1,21 @@
+﻿namespace HITSBlazor.Components.LeftSideNavigation
+{
+    public class NavigationItem(string title, string icon, int id, List<NavigationSubItem> subItems)
+    {
+        public string Title { get; } = title;
+        public string Icon { get; } = icon;
+        public int Id { get; } = id;
+        public List<NavigationSubItem> SubItems { get; } = subItems;
+        public bool IsExpanded { get; set; }
+    }
+
+    public class NavigationSubItem(
+        string title, string icon, int id
+    )
+    {
+        public string Title { get; } = title;
+        public string Icon { get; } = icon;
+        public int Id { get; } = id;
+        public bool IsActive { get; set; }
+    }
+}
---
+++ HITSBlazor/Components/LeftSideNavigation/NavigationStyles.cs
@@ -0,0 +1,38 @@
+﻿namespace HITSBlazor.Components.LeftSideNavigation
+{
+    public static class NavigationStyles
+    {
+        public const string Layout = "height: 100vh; width: 85px; min-width: 85px; max-width: 85px;";
+
+        public static string GetNavigationStyle() => string.Join("; ", new[]
+        {
+            "position: relative",
+            "top: auto",
+            "right: auto",
+            "bottom: auto",
+            "left: auto",
+            "z-index: 8",
+            "overflow-x: hidden",
+            "display: flex",
+            "flex-direction: column",
+            "flex-wrap: nowrap",
+            "align-items: stretch",
+            "justify-content: space-between",
+            "gap: 8px",
+            "transition: all 0.15s ease-out;"
+        });
+
+        public static string GetNavigationContentStyle() => string.Join("; ", new[]
+        {
+            "width: 280px",
+            "min-width: 280px",
+            "max-width: 280px",
+            "display: flex",
+            "flex-direction: column",
+            "flex-wrap: nowrap",
+            "align-items: flex-start",
+            "justify-content: flex-start",
+            "gap: 8px;"
+        });
+    }
+}
