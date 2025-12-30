a3c5a7bc9ffcfa5eb7d28678e2956cbb02002ee9
Flakoooo <vlasov.vadom@gmail.com>
2025-12-30 14:27:52 +0500

    page header + start create ideas page

---
+++ HITSBlazor/Pages/Ideas/IdeasList.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace HITSBlazor.Pages.IdeasList
+{
+    [Authorize]
+    [Route("/ideas/list")]
+    public partial class IdeasList
+    {
+
+    }
+}
