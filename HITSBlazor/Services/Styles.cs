23b097d8fc355ca77e61cf7c07dde2ba8c9208ed
Flakoooo <vlasov.vadom@gmail.com>
2026-01-02 19:35:14 +0500

    ideas table UI complete, need functions

---
+++ HITSBlazor/Services/Styles.cs
@@ -0,0 +1,8 @@
+﻿namespace HITSBlazor.Services
+{
+    public static class Styles
+    {
+        public static string Btn => "flex-direction: row; flex-wrap: nowrap; align-items: center; justify-content: center; gap: 4px;";
+        public static string Bi => "flex-direction: row; align-items: center; justify-content: center; display: flex; flex-wrap: nowrap;";
+    }
+}
